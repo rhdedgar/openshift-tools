da74adc35ae22112570c643b34faa06069c9d542
Doug Edgar <dedgar@redhat.com>
2017-10-31 12:41:00 -0400

    clam scanner pod 6

--- docker/oso-clam-scanner/src/start.sh
+++ docker/oso-clam-scanner/src/start.sh
@@ -3,14 +3,14 @@
 # This is useful so we can debug containers running inside of OpenShift that are
 # failing to start properly.
 
-#if [ "$OO_PAUSE_ON_START" = "true" ] ; then
-echo
-echo "This container's startup has been paused indefinitely because OO_PAUSE_ON_START has been set."
-echo
-while true; do
-  sleep 10    
-done
-#fi
+if [ "$OO_PAUSE_ON_START" = "true" ] ; then
+  echo
+  echo "This container's startup has been paused indefinitely because OO_PAUSE_ON_START has been set."
+  echo
+  while true; do
+    sleep 10    
+  done
+fi
 
 echo "Running config playbook"
 ansible-playbook /root/config.yml
@@ -19,5 +19,6 @@ ansible-playbook /root/config.yml
 echo
 echo 'ClamAV scan'
 echo '----------------'
+sleep 300
 /bin/clam-scanner scan --path=/host/proc/ --omit-negative-results
 echo
