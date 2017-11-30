fde6525c5bc4740c14dab828b1c41642fee4ea13
Doug Edgar <dedgar@redhat.com>
2017-11-30 18:34:36 -0500

    workflow updates

--- docker/oso-image-inspector/rhel7/scripts/cron-scan-pods.sh
+++ docker/oso-image-inspector/rhel7/scripts/cron-scan-pods.sh
@@ -2,4 +2,6 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-for line in $(chroot /host /usr/bin/docker ps -q); do echo "$line" && image-inspector -scan-type=clamav -clam-socket=/host/run/clamd.scan/clamd.sock -container="$line" -post-results-url http://localhost:8080; done
+for line in $(chroot /host /usr/bin/docker ps | grep -v 'host-monitoring\|logging-fluentd\|image-inspector\|CONTAINER ID' | awk '{print $1}'); do echo "$line" && image-inspector -scan-type=clamav -clam-socket=/host/run/clamd.scan/clamd.sock -container="$line" -post-results-url http://localhost:8080; done
+
+/usr/local/bin/upload_scanlogs
