b1f1894cf66df776500417835358a843ef8a11fe
Doug Edgar <dedgar@redhat.com>
2018-03-01 12:43:09 -0500

    adjustments and cron updates 10

--- docker/oso-evg-query/centos7/scripts/start.sh
+++ docker/oso-evg-query/centos7/scripts/start.sh
@@ -15,6 +15,9 @@ fi
 echo user:x:$(id -u):0:USER:/root:/bin/bash >> /etc/passwd
 echo group:x:$(id -G | awk '{print $2}'):user >> /etc/group
 
+echo "Running config playbook"
+ansible-playbook /root/config.yml
+
 echo This container hosts the following applications:
 echo
 echo '/root/mysql_query'
